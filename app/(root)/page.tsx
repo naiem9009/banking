1676a5d60639878516bb13209167c127e4e399cf
naiem9009 <naiem9009@gmail.com>
2024-08-03 20:43:09 +0600

    changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,34 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {
+    firstName : "Naim",
+    lastName: "Hossen",
+    email: "email@gmail.com"
+  }
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+        </header>
+
+        RECENT TRANSACTION
+      </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        banks={[{}, {}]}
+      />
+
+    </section>
   )
 }
 
